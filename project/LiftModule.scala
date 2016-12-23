ebc5563994e91a01b0fb0dcb5c4693f91af40d86
Rike-Benjamin Schuppner <debilski@debilski.de>
2016-12-23 13:51:13 +0100

    Lift 3.0 (#21)
    * Use sbt 0.13.13.
    
    * Update sbt syntax and use Lift 3.0.1
    
    * Get credentials from env.

--- project/LiftModule.scala
+++ project/LiftModule.scala
@@ -1,13 +1,7 @@
 import sbt._
 import sbt.Keys._
 
-object LiftModuleBuild extends Build {
-
-  val liftVersion = SettingKey[String]("liftVersion", "Full version number of the Lift Web Framework")
-
-  val liftEdition = SettingKey[String]("liftEdition", "Lift Edition (short version number to append to artifact name)")
-
-  val project = Project("LiftModule", file("."))
-
+object LiftModule {
+  val liftVersion = settingKey[String]("Lift Web Framework full version number")
+  val liftEdition = settingKey[String]("Lift Edition (such as 2.6 or 3.0)")
 }
-
